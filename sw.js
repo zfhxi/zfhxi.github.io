5ad94dbbc0cad6e28e8772698686c6d610043df7
root <root@ip-172-31-52-192.us-west-2.compute.internal>
2021-05-12 12:40:11 +0000

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-a26d5a12bff015912cc4.js"
+    "url": "webpack-runtime-fbd011f0099da12a5ea1.js"
   },
   {
     "url": "framework-02fcab78320a77685ff9.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "ab89acd32504fdb2f47e12b53f403e96"
+    "revision": "346791f3e3fb67c07e118e84733a4794"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-0f973a712823551132fd.js"
